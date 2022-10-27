82d06997ed5a338f62843263aed5dd664e8ae266
Oscar Caicedo <oscarccd17@gmail.com>
2022-10-27 14:22:31 -0500

    printing out maze with nested enum loop

--- path-finder.py
+++ path-finder.py
@@ -1,7 +1,7 @@
 import curses
 from curses import wrapper
+import enum
 import queue
-from sys import stderr
 import time
 
 maze = [
@@ -16,10 +16,20 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze): # Grabs the index of full row.
+        for j, value in enumerate(row): # Grabs the column index and value of every row.
+            stdscr.addstr(i, j, value) # Grabs index, column, and value of Maze.
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
     stdscr.clear()
-    stdscr.addstr(0, 0, "hello world!")
+    print_maze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
 
