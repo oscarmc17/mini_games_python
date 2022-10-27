791cf371f0989bc647079c2afae172193daa9efb
Oscar Caicedo <oscarccd17@gmail.com>
2022-10-27 13:40:48 -0500

    maze and import statements

---
+++ path-finder.py
@@ -0,0 +1,27 @@
+import curses
+from curses import wrapper
+import queue
+from sys import stderr
+import time
+
+maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
+
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr(0, 0, "hello world!")
+    stdscr.refresh()
+    stdscr.getch()
+
+
+wrapper(main)
