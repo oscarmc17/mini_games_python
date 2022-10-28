e732d788a8cb823799790e36e51dbf7f8af6a407
Oscar Caicedo <oscarccd17@gmail.com>
2022-10-27 22:04:57 -0500

    updated definitions and example enumerate print statement

--- path-finder.py
+++ path-finder.py
@@ -20,18 +20,24 @@ def print_maze(maze, stdscr, path=[]):
     BLUE = curses.color_pair(1)
     RED = curses.color_pair(2)
 
-    for i, row in enumerate(maze): # Grabs the index of full row.
-        for j, value in enumerate(row): # Grabs the column index and value of every row.
-            stdscr.addstr(i, j, value) # Grabs index, column, and value of Maze.
+    for i, row in enumerate(maze):  # Grabs the index of full row.
+        for j, value in enumerate(row):  # Grabs the column index and value of every row.
+            stdscr.addstr(i, j, value)  # Grabs index, column, and value of Maze.
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
-    stdscr.clear()
+    stdscr.clear()  # Clear screen
     print_maze(maze, stdscr)
-    stdscr.refresh()
-    stdscr.getch()
+    stdscr.refresh()  # Refreshes the screen
+    stdscr.getch()  # Get Character. Waits until user hits a key to exit the program.
 
 
 wrapper(main)
+
+
+arr = ["lambo", "nissan", "bmw", "porsche"]
+
+for i, brand in enumerate(arr):
+    print(i, brand)
