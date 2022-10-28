1476b856a09066f14e433cca51171646748a56b5
Oscar Caicedo <oscarccd17@gmail.com>
2022-10-27 22:25:50 -0500

    create find_start function to find where maze starts and where it equals a specific value

--- path-finder.py
+++ path-finder.py
@@ -22,7 +22,21 @@ def print_maze(maze, stdscr, path=[]):
 
     for i, row in enumerate(maze):  # Grabs the index of full row.
         for j, value in enumerate(row):  # Grabs the column index and value of every row.
-            stdscr.addstr(i, j, value)  # Grabs index, column, and value of Maze.
+            stdscr.addstr(i, j*2, value, BLUE)  # Grabs index, column, and value of Maze.
+
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j
+    return None
+
+def find_path(maze, stdscr):
+    start = "O"
+    end = "X"
+    start_pos = find_start(maze, start)
+
+    q = queue.Queue()
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
@@ -37,7 +51,7 @@ def main(stdscr):
 wrapper(main)
 
 
-arr = ["lambo", "nissan", "bmw", "porsche"]
+# arr = ["lambo", "nissan", "bmw", "porsche"]
 
-for i, brand in enumerate(arr):
-    print(i, brand)
+# for i, brand in enumerate(arr):
+#     print(i, brand)
