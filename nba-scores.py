692c184369ec6e0437bc51774e64e32177765743
Oscar Caicedo <oscarccd17@gmail.com>
2022-11-08 21:41:27 -0600

    creating link and scoreboard functions

--- nba-scores.py
+++ nba-scores.py
@@ -3,3 +3,19 @@ from pprint import PrettyPrinter
 
 BASE_URL = "https://data.nba.net"
 ALL_JSON = "/prod/v1/today.json"
+
+printer = PrettyPrinter()
+
+def get_links():
+    data = get(BASE_URL + ALL_JSON).json()
+    links = data['links']
+    return links
+
+
+def get_scoreboard():
+    scoreboard = get_links()['currentScoreboard']
+    data = get(BASE_URL + scoreboard).json()
+
+    printer.pprint(data.keys())
+
+get_scoreboard()
